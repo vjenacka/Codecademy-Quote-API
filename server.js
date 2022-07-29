89ed8afbd5c0b7166a74a699afafd603001f96f0
vjenacka <edins613@gmail.com>
2022-07-29 20:47:24 +0200

    finished basic project requirement (get for random quotes/all quotes/quotes by person name) and adding new quote

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.listen(PORT, () => {
+  console.log(`Listening on port:${PORT}`);
+});
+
+//random quote route
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = { quote: getRandomElement(quotes) };
+
+  randomQuote
+    ? res.send(randomQuote)
+    : res.status(404).send("No random quote!");
+});
+
+//gets all the quotes, get quotes by persons name
+app.get("/api/quotes", (req, res, next) => {
+  if (Object.keys(req.query).length === 0) {
+    res.send({ quotes });
+  }
+
+  const { person } = req.query;
+  const newQuotes = quotes.filter(quote => quote.person === person);
+  if (newQuotes.length !== 0) {
+    res.send({ quotes: newQuotes });
+  } else {
+    res.status(404).send("No person with that quote");
+  }
+});
+
+//add new quotes
+app.post("/api/quotes", (req, res, next) => {
+  const newQuote = { quote: req.query.quote, person: req.query.person };
+
+  if (req.query.quote !== "" && req.query.person !== "") {
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400);
+  }
+});
